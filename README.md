af01f95b688a07b2d3e52058a1b4c92305c1e26a
Pavel Filatov <filatov.py@gmail.com>
2023-02-15 22:19:50 +0100

    [01] Introduce partitioning and describe how to check outputs

--- README.md
+++ README.md
@@ -32,7 +32,30 @@ Example usages:
 ```shell
 # Parquet is used by default
 poetry run gostudent -i data/input -o data/output/salaries.parquet
+```
 
-# Store as csv to look at the data
-poetry run gostudent -i data/input -o data/output/salaries.csv
+### How to check the outputs
+
+As soon as Parquet is not a human-readable format, we need something else to check how the 
+outputs are created. 
+
+The solution is storing data in CSV format. 
+
+```shell
+poetry run gostudent -i data/input -o data/output/salaries.csv -of csv
+```
+
+In this case, you can go to the output location and check them manually, e.g.:
+
+```shell
+head data/output/salaries.csv/2021-08/2021-08-16.csv
+```
+
+
+### Run test 
+
+To run the tests, execute:
+
+```shell
+poetry run pytest
 ```
--- gostudent.py
+++ gostudent.py
@@ -1,5 +1,8 @@
+import logging
 from argparse import ArgumentParser
+from datetime import datetime
 from pathlib import Path
+from typing import List, Tuple
 
 import pandas as pd
 
@@ -23,12 +26,17 @@ def main() -> None:
     # Transform
     salaries = transform(lessons_info_raw, tutors_info_raw)
 
+    salary_tables_per_date = filter_data_and_assign_month_date(salaries)
+
     # IO: Write
-    output_base_dir.mkdir(parents=True, exist_ok=True)
-    if output_file_format == "csv":
-        salaries.to_csv(output_base_dir / "salaries.csv", index=False)
-    if output_file_format == "parquet":
-        salaries.to_parquet(output_base_dir / "salaries.parquet", index=False)
+    for month, date, table in salary_tables_per_date:
+        write(table, output_base_dir / month / f"{date}.{output_file_format}", output_file_format)
+
+
+def filter_data_and_assign_month_date(salaries: pd.DataFrame) -> List[Tuple[str, str, pd.DataFrame]]:
+    dates = salaries["date"].unique()
+    parse_month = lambda d: datetime.strptime(d, "%Y-%m-%d").strftime("%Y-%m")
+    return [(parse_month(d), d, salaries[salaries["date"] == d]) for d in dates]
 
 
 def build_parser() -> ArgumentParser:
@@ -41,6 +49,19 @@ def build_parser() -> ArgumentParser:
     )
 
     return p
+
+
+def write(df: pd.DataFrame, output_path: Path, file_format: str) -> None:
+    logging.info(f"Writing table under {output_path} as {file_format}.")
+    output_path.parent.mkdir(parents=True, exist_ok=True)
+    if file_format == "csv":
+        df.to_csv(output_path, index=False)
+    elif file_format == "json":
+        df.to_json(output_path, orient="records")
+    elif file_format == "parquet":
+        df.to_parquet(output_path, index=False)
+    else:
+        raise RuntimeError("Wrong output file format!")
 
 
 if __name__ == "__main__":
